6a9dfd64fad4995a1b90d6b96abd5f9f377a86de
marku vinicius <markuvinicius@gmail.com>
2023-03-04 21:53:06 +0000

    Fixed jsonproperty for UserDTO.name field

--- src/main/java/com/markuvinicius/twitter/stream/dtos/json/UserDTO.java
+++ src/main/java/com/markuvinicius/twitter/stream/dtos/json/UserDTO.java
@@ -8,8 +8,24 @@ public class UserDTO {
     @JsonProperty
     private double id;
 
-    @JsonInclude
+    @JsonProperty
     private String name;
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     //TODO: Add missing fields to DTO
 
